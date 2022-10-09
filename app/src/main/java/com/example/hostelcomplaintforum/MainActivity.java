b949d335d606716077ff12563f885731f158b1ee
Rohit Agrawal <64243388+rohit1508-gif@users.noreply.github.com>
2022-10-09 22:01:54 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/hostelcomplaintforum/MainActivity.java
+++ app/src/main/java/com/example/hostelcomplaintforum/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //app will not be affected by dark mode
+        
         initializations();
         btnFuncs();
 
